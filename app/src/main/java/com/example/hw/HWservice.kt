a428a89d2fe83090048732ffb302e8eafd2c26c0
Naerrean <naer168@gmail.com>
2021-08-17 12:25:11 +0500

    First commit

--- app/src/main/java/com/example/hw/HWservice.kt
+++ app/src/main/java/com/example/hw/HWservice.kt
@@ -7,6 +7,6 @@ import android.os.IBinder
 class HWservice : Service() {
 
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        TODO("Return XXX the communication channel to the service.")
     }
 }
