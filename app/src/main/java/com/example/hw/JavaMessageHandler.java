16fc1c4c326f8345535cdc0fe8df1c79dffed941
naer1 <naer168@gmail.com>
2021-09-03 11:37:17 +0500

    Inity Connect

---
+++ app/src/main/java/com/example/hw/JavaMessageHandler.java
@@ -0,0 +1,5 @@
+package com.example.hw;
+
+public interface JavaMessageHandler {
+    void onMessage(String message, String data);
+}
---
+++ app/src/main/java/com/example/hw/UnityBridge.java
@@ -0,0 +1,40 @@
+package com.example.hw;
+
+import android.os.Handler;
+
+public final class UnityBridge {
+    // Содержит ссылку на C# реализацию интерфейса
+    private static JavaMessageHandler javaMessageHandler;
+    // Перенаправляет вызов в Unity поток
+    private static Handler unityMainThreadHandler;
+
+    public static void registerMessageHandler(JavaMessageHandler handler) {
+        javaMessageHandler = handler;
+        if(unityMainThreadHandler == null) {
+            // Так как эту функцию вызываем всегда на старте Unity,
+            // этот вызов идет из нужного нам в дальнейшем потока,
+            // создадим для него Handler
+            unityMainThreadHandler = new Handler();
+        }
+    }
+
+    // Функция перевода выполнения в Unity поток, потребуется в дальнейшем
+    public static void runOnUnityThread(Runnable runnable) {
+        if(unityMainThreadHandler != null && runnable != null) {
+            unityMainThreadHandler.post(runnable);
+        }
+    }
+
+    // Пишем какую-нибудь функцию, которая будет отправлять сообщения в Unity
+    public static void SendMessageToUnity(final String message, final String data) {
+        runOnUnityThread(new Runnable() {
+
+            @Override
+            public void run() {
+                if(javaMessageHandler != null) {
+                    javaMessageHandler.onMessage(message, data);
+                }
+            }
+        });
+    }
+}
